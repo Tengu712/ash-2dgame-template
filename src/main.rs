e18bf04cea6ea0d85468f9fe50628294f76d3eb1
Tengu712 <genreihoutengu@gmail.com>
2025-12-25 14:15:06 +0900

    add ash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use ash::{Entry, Instance, vk};
+
+fn create_instance(entry: &Entry) -> Instance {
+    let ai = vk::ApplicationInfo::default()
+        .application_name(c"ash-2dgame-template")
+        .application_version(0)
+        .api_version(vk::make_api_version(0, 1, 0, 0));
+    let ci = vk::InstanceCreateInfo::default().application_info(&ai);
+    unsafe {
+        entry
+            .create_instance(&ci, None)
+            .expect("Instance creation error")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let entry = unsafe { Entry::load() }.expect("Failed to load Vulkan loader");
+    let instance = create_instance(&entry);
+    unsafe { instance.destroy_instance(None) };
 }
