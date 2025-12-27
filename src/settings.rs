1ce67f2cc450ecf67b506b5e9fc1b1ae361989b8
Tengu712 <genreihoutengu@gmail.com>
2025-12-27 18:21:40 +0900

    make version

--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,7 @@
 //! アプリケーションの定数を羅列するモジュール
 
+use ash::vk;
 use std::ffi::CStr;
 
 pub const APPLICATION_NAME: &CStr = c"ash-2dgame-template";
-pub const APPLICATION_VERSION: u32 = 0;
+pub const APPLICATION_VERSION: u32 = vk::make_api_version(0, 0, 1, 0);
