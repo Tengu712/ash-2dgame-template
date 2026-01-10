a504a97a58a7832988c5ab362d64b758e9520907
Tengu712 <genreihoutengu@gmail.com>
2026-01-10 19:19:18 +0900

    fix licenses

---
+++ licenses/build.py
@@ -0,0 +1,62 @@
+"""
+サードパーティライセンスを生成するスクリプト
+
+Cargo由来のソフトウェアとadditional.jsonに記したソフトウェアを対象とする。
+LICENSES.txtが生成される。
+
+前提条件:
+- cargo-aboutをインストールしておくこと
+- このファイルと同じディレクトリで実行すること
+"""
+
+import json
+import platform
+import subprocess
+from pathlib import Path
+
+current_platform = platform.system()
+
+TARGETS = {
+	'Windows': 'x86_64-pc-windows-msvc',
+}
+
+target = TARGETS.get(current_platform)
+if not target:
+	raise RuntimeError(f'unsupported os: {current_platform}')
+
+output = subprocess.run(
+	[
+		'cargo', 'about', 'generate',
+		'--manifest-path', '../Cargo.toml',
+		'--config', 'about.toml',
+		'--target', target,
+		'--format', 'json',
+	],
+	capture_output=True, text=True, encoding='utf-8', check=True
+).stdout
+
+cargo_about_data = json.loads(output)
+additional_data = json.loads(Path('additional.json').read_text('utf8'))
+
+deps = additional_data.copy()
+
+for license in cargo_about_data['licenses']:
+	for crate in license['used_by']:
+		deps.append({
+			'name': crate['crate']['name'],
+			'version': crate['crate']['version'],
+			'text': license['text'],
+		})
+
+deps.sort(key=lambda d: d['name'])
+
+result = '# Licenses\n\n'
+
+for dep in deps:
+	if dep['name'] == 'xorg' and current_platform != 'Linux':
+		continue
+	result += f"## {dep['name']} {dep['version']}\n\n{dep['text']}\n\n\n"
+
+result = result.rstrip() + '\n'
+
+Path('LICENSES.txt').write_text(result, 'utf8')
